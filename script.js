2549efe26e56aa01f8baa33e51e7b1cff32692d5
Jordan B <Jikao55@outlook.fr>
2023-10-12 12:28:51 +0200

    Générateur de couleurs terminé.

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-const body = document.querySelector("body")
-let color1 = ""
-let color2 = ""
-let color3 = ""
-
-const autoColor = (color1, color2, color3) => {
+function autoColor() {
     
-    color1 = Math.floor(Math.random() * 255);
-    color2 = Math.floor(Math.random() * 255);
-    color3 = Math.floor(Math.random() * 255);
+    let color1 = Math.floor(Math.random() * 255);
+    let color2 = Math.floor(Math.random() * 255);
+    let color3 = Math.floor(Math.random() * 255);
     
-    body.style.background = `rgb(${color1}, ${color2}, ${color3})`;
+    document.body.style.background = `rgb(${color1}, ${color2}, ${color3})`;
     span.textContent = `rgb(${color1}, ${color2}, ${color3})`;
     
 }
 
-setInterval(() => {
-    autoColor()
-}, 2000);
+setInterval(autoColor, 2000);
